cbf9e600e8869c2b37fca70b670cbf71e871aca4
Nikita Savitskyi <niikiitaa03@gmail.com>
2019-03-26 22:43:28 +0300

    codestyle fixes

---
+++ DEV-2/DEV-2/SpecialLetter.cs
@@ -0,0 +1,16 @@
+﻿namespace DEV_2
+{
+    struct Special
+    {
+        public string sound;
+
+        /// <summary>
+        /// Fills the fields with default characteristics 
+        /// </summary>
+        /// <param name="letter">Letter from the received word</param>
+        public Special(char letter)
+        {
+            sound = letter == 'ь' ? "'" : string.Empty;
+        }
+    }
+}
---
+++ DEV-2/DEV-2/Vowel.cs
@@ -0,0 +1,33 @@
+﻿namespace DEV_2
+{
+    struct Vowel
+    {
+        public string sound;
+        public bool isStressed, isIoated, addJSound;
+
+        /// <summary>
+        /// Fills the fields with default characteristics 
+        /// </summary>
+        /// <param name="letter">Letter from the received word</param>
+        public Vowel(char letter)
+        {
+            isStressed = letter == 'ё';
+            isIoated = Letter.vowelIonationEquivalents.ContainsKey(letter);
+            addJSound = true;
+            sound = isIoated ? Letter.vowelIonationEquivalents[letter].ToString() : letter.ToString();
+        }
+
+        public void Update()
+        {
+            //updates with necessary changes
+            if (sound == "о" && !isStressed)
+            {
+                sound = "а";
+            }
+            else if (isIoated)
+            {
+                sound = addJSound ? ("й" + sound) : ("'" + sound);
+            }
+        }
+    }
+}
