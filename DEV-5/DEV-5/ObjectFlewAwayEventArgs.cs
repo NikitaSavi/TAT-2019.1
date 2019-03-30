e7ad8e63efa27408853793614f944a203be185f5
Nikita Savitskyi <niikiitaa03@gmail.com>
2019-03-30 22:07:13 +0300

    comments for <object...args>.cs

--- DEV-5/DEV-5/ObjectFlewAwayEventArgs.cs
+++ DEV-5/DEV-5/ObjectFlewAwayEventArgs.cs
@@ -2,11 +2,18 @@
 
 namespace DEV_5
 {
+    /// <inheritdoc />
+    ///  <summary>
+    ///  Class contains arguments for the ObjectFlewAway event: time and speed
+    ///  </summary>
     public class ObjectFlewAwayEventArgs : EventArgs
     {
         public double Time { get; set; }
         public double Speed { get; set; }
 
+        /// <inheritdoc />
+        /// <param name="time">Time of the flight</param>
+        /// <param name="speed">Speed of the flight</param>
         public ObjectFlewAwayEventArgs(double time, int speed)
         {
             Time = time;
